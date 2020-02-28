c8e1152f32ca439345c7fd8eee36f16536d73531
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-02-27 21:11:01 -0500

    imported counter into App.js

--- src/Counter.js
+++ src/Counter.js
@@ -11,7 +11,7 @@ class Counter extends React.Component{
 
     decrement = () => {
         this.setState({
-            count: this.state.count-1;
+            count: this.state.count-1
         });
     }
 
@@ -28,3 +28,5 @@ class Counter extends React.Component{
         )
     }
 }
+
+export default Counter;
