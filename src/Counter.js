acf8f82444027e0ce64d4565a334d1cf2ab7be83
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-03-01 21:37:43 -0500

    fully working with mapDispatchToProps

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {INCREMENT, DECREMENT} from './actions';
+import {increment, decrement} from './actions';
 
 class Counter extends React.Component{
     //state = {count: 0}
@@ -9,14 +9,18 @@ class Counter extends React.Component{
         // this.setState({
         //     count: this.state.count+1
         // });
-        this.props.dispatch({type: INCREMENT});
+        //this.props.dispatch({type: INCREMENT}); //initially
+        //this.props.dispatch(increment()); //after adding action creator
+        this.props.increment(); //after mapDispatchToProps
     }
 
     decrement = () => {
         // this.setState({
         //     count: this.state.count-1
         // });
-        this.props.dispatch({type: DECREMENT});
+        //this.props.dispatch({type: DECREMENT}); 
+        //this.props.dispatch(decrement()); 
+        this.props.decrement();
     }
 
     render() {
@@ -38,15 +42,22 @@ class Counter extends React.Component{
     }
 }
 
-//Add this function:
 function mapStateToProps(state){
     return{
         count: state.count
     };
 }
 
+// in this object, keys become prop names,
+// and values should be action creator functions.
+// They get bound to `dispatch`. 
+const mapDispatchToProps={
+    increment,
+    decrement
+};
+
 //replace this:
 //export default Counter;
 
 //with this:
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
